95ed935fcba5088b45c14c5a3358c9e2fa5f24f2
Nguyen Tien Hoang <nguyentienhoang139@gmail.com>
2017-04-17 11:14:47 +0700

    tạo project

---
+++ MinhToan-Manager/MinhToan-Manager/MyForm/frm_Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace MinhToan_Manager.MyForm
+{
+    public partial class frm_Main : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        bool minimizedToTray;
+        NotifyIcon notifyIcon;
+
+        #region Chỉ hiển chạy 1 form
+        protected override void WndProc(ref Message message)
+        {
+            if (message.Msg == SingleInstance.WM_SHOWFIRSTINSTANCE)
+            {
+                ShowWindow();
+            }
+            base.WndProc(ref message);
+        }
+        private void btnMinToTray_Click(object sender, EventArgs e)
+        {
+            // Tie this function to a button on your main form that will minimize your
+            // application to the notification icon area (aka system tray).
+            MinimizeToTray();
+        }
+        void MinimizeToTray()
+        {
+            notifyIcon = new NotifyIcon();
+            //notifyIcon.Click += new EventHandler(NotifyIconClick);
+            notifyIcon.DoubleClick += new EventHandler(NotifyIconClick);
+            notifyIcon.Icon = this.Icon;
+            notifyIcon.Text = ProgramInfo.AssemblyTitle;
+            notifyIcon.Visible = true;
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+            minimizedToTray = true;
+        }
+        public void ShowWindow()
+        {
+            if (minimizedToTray)
+            {
+                notifyIcon.Visible = false;
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                minimizedToTray = false;
+            }
+            else
+            {
+                WinApi.ShowToFront(this.Handle);
+            }
+        }
+        void NotifyIconClick(Object sender, System.EventArgs e)
+        {
+            ShowWindow();
+        }
+
+        #endregion
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+       
+    }
+}
---
+++ MinhToan-Manager/MinhToan-Manager/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.UserSkins;
+using DevExpress.Skins;
+using MinhToan_Manager.MyForm;
+
+namespace MinhToan_Manager
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            if (!SingleInstance.Start())
+            {
+                SingleInstance.ShowFirstInstance();
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            SkinManager.EnableFormSkins();
+
+            try
+            {
+                frm_Main mainForm = new frm_Main();
+                Application.Run(mainForm);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            SingleInstance.Stop();
+        }
+    }
+}
